6ffb3e10781b3552586b138818cdc88f163c4b98
SongWenqi <Wendy@SongWenqis-MacBook-Air.local>
2017-02-05 22:36:06 -0800

    …hahaha… forgot to save files first

--- Assets/Lift.cs
+++ Assets/Lift.cs
@@ -4,13 +4,5 @@ using UnityEngine;
 
 public class Lift : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	private 
 }
